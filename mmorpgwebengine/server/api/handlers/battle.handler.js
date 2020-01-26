afa95240d40a599d46e060e06e4f5453c36c3773
Alexander Podkatilov <apodkatilov@gmail.com>
2020-01-26 14:29:48 +0300

    add event manager

--- mmorpgwebengine/server/api/handlers/battle.handler.js
+++ mmorpgwebengine/server/api/handlers/battle.handler.js
@@ -1,4 +1,4 @@
-import { Mongoose } from mongoose from 'mongoose';
+import mongoose from 'mongoose';
 import Battle from '../../../models/battle';
 import Response from '../response';
 import BattleEventManager, { BattleEvents } from '../../core/playerGameContext';
@@ -30,20 +30,20 @@ export const connect = (req, res) => {
   const { context } = req;
 
   Battle.connect(context.player, battleId, teamId)
-  .then((battle) => {
+    .then((battle) => {
     // eslint-disable-next-line no-underscore-dangle
-    BattleEventManager.signalConnectBattle(battleId, teamId, context.player._id);
-    return mongoose.Promise.resolve(battle);
-  })
-  .then((battle) => {
-    res.status(200).json(
-      new Response(true, battle.toObject()).toObject(),
-    );
-  }).catch((err) => res
-    .status(400)
-    .json(
-      new Response(false, null, `Ошибка подключения к битве (${err.message})`),
-    ));
+      BattleEventManager.signalConnectBattle(battleId, teamId, context.player._id);
+      return mongoose.Promise.resolve(battle);
+    })
+    .then((battle) => {
+      res.status(200).json(
+        new Response(true, battle.toObject()).toObject(),
+      );
+    }).catch((err) => res
+      .status(400)
+      .json(
+        new Response(false, null, `Ошибка подключения к битве (${err.message})`),
+      ));
 };
 
 export const getActive = (req, res) => {
