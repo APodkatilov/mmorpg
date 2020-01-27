55791248f7001692a0d3b51ed1a03c493f78c738
Alexander Podkatilov <apodkatilov@gmail.com>
2020-01-28 01:34:03 +0300

    deploy

--- mmorpgwebengine/config.js
+++ mmorpgwebengine/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   env: process.env.NODE_ENV,
   host: process.env.HOST || '127.0.0.1',
-  port: process.env.PORT || (process.env.NODE_ENV === 'production' ? 8080 : 3001),
+  port: process.env.UIPORT || (process.env.NODE_ENV === 'production' ? 8080 : 3001),
   apiHost: process.env.APIHOST || '127.0.0.1',
-  apiPort: process.env.APIPORT || '3030',
+  apiPort: process.env.PORT || '3030',
   webSocketPort: process.env.WSPORT || '3031',
   dbHost: 'localhost',
   dbPort: '27017',
